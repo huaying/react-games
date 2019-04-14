1ab819add2cad3d5d8df219a2da499f5e9bde2a2
huaying <royal3501@gmail.com>
2019-04-14 16:48:49 -0700

    tic tac toe: implement game logic

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -9,10 +9,25 @@ function Block({ pos, grid, onClick }) {
   );
 }
 
+const GameStatus = {
+  Init: "Init",
+  Playing: "Playing",
+  End: "End"
+};
+
 class App extends Component {
   state = {
     grid: Array(9).fill(null),
-    turn: "O"
+    turn: "O",
+    gameStatus: GameStatus.Init
+  };
+
+  check = {
+    rows: Array(3).fill(0),
+    cols: Array(3).fill(0),
+    diagonal: 0,
+    antiDiagonal: 0,
+    total: 0
   };
 
   move = pos => {
@@ -20,10 +35,40 @@ class App extends Component {
 
     if (grid[pos] === null) {
       const newGrid = [...grid];
+      const gameStatus = this.gameCheck(pos);
+      console.log(gameStatus);
 
       newGrid[pos] = turn;
-      this.setState({ grid: newGrid, turn: turn === "O" ? "X" : "O" });
+      this.setState({
+        grid: newGrid,
+        turn: turn === "O" ? "X" : "O",
+        gameStatus
+      });
     }
+  };
+
+  gameCheck = pos => {
+    const add = this.state.turn === "O" ? 1 : -1;
+    const row = Math.floor(pos / 3);
+    const col = Math.floor(pos % 3);
+
+    this.check.rows[row] += add;
+    this.check.cols[col] += add;
+    if ([0, 4, 8].includes(pos)) this.check.diagonal += add;
+    if ([2, 4, 6].includes(pos)) this.check.antiDiagonal += add;
+    this.check.total++;
+
+    if (
+      Math.abs(this.check.rows[row]) === 3 ||
+      Math.abs(this.check.cols[col]) === 3 ||
+      Math.abs(this.check.diagonal) === 3 ||
+      Math.abs(this.check.antiDiagonal) === 3 ||
+      this.check.total === 9
+    ) {
+      return GameStatus.End;
+    }
+
+    return GameStatus.Playing;
   };
 
   render() {
