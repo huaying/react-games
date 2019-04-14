250ec4868f95fb81ae29a3186caf0dfc9b036fe0
huaying <royal3501@gmail.com>
2019-04-14 15:33:06 -0700

    tic tac toe: add basic ui

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,9 +1,39 @@
 import React, { Component } from "react";
 import "./App.css";
 
+function Block({ value }) {
+  return <div className="block">{value}</div>;
+}
+
 class App extends Component {
+  state = {
+    grid: Array(9).fill("O")
+  };
+
   render() {
-    return <div />;
+    const { grid } = this.state;
+
+    return (
+      <div className="container">
+        <div className="box">
+          <div className="row">
+            <Block value={grid[0]} />
+            <Block value={grid[1]} />
+            <Block value={grid[2]} />
+          </div>
+          <div className="row">
+            <Block value={grid[3]} />
+            <Block value={grid[4]} />
+            <Block value={grid[5]} />
+          </div>
+          <div className="row">
+            <Block value={grid[6]} />
+            <Block value={grid[7]} />
+            <Block value={grid[8]} />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
